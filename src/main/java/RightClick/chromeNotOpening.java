392334f8f2138ae249f87b8c823cdbf303129328
akthar <aktharniyaz90427@gmail.com>
2023-05-30 08:08:43 +0530

    commit sms this is

--- src/main/java/RightClick/chromeNotOpening.java
+++ src/main/java/RightClick/chromeNotOpening.java
@@ -5,10 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class chromeNotOpening {
 	public static void main(String[] args) {
+		
 		WebDriver driver = new ChromeDriver();
 		driver.get("www.faceook.com");
 		System.out.println("after uploading git 8 am");
 		System.out.println("second time");
+		System.out.println("thord time git check");
 	}
 
 }
