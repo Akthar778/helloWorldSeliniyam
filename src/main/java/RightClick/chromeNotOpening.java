b7e3a58e7e35a15ed8bdccd49f606590693d29e3
akthar <aktharniyaz90427@gmail.com>
2023-05-30 08:03:07 +0530

    second time add

--- src/main/java/RightClick/chromeNotOpening.java
+++ src/main/java/RightClick/chromeNotOpening.java
@@ -7,6 +7,8 @@ public class chromeNotOpening {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.get("www.faceook.com");
+		System.out.println("after uploading git 8 am");
+		System.out.println("second time");
 	}
 
 }
